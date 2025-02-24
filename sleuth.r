73be74dc123b1180895383288c587735794dffff
Lila Nelson <lnelson5452@gmail.com>
2025-02-24 13:15:46 -0600

    I added comments to this script

--- sleuth.r
+++ sleuth.r
@@ -1,15 +1,19 @@
 library(sleuth)
 library(dplyr)
 
-
+# reading in the table that is a part of the repo that has the accession numbers and correlating conditions and paths for the kallisto output
 stab = read.table("Pipeline_Project/sleuth_input",header=TRUE)
 
+# initializing sleuth with the table 
 so = sleuth_prep(stab)
 
+# this command fits a model for the conditions
 so = sleuth_fit(so, ~condition, 'full')
 
+# this command fits the reduced model
 so = sleuth_fit(so, ~1, 'reduced')
 
+# this command performs the liklihood ratio tests for the full and reduced fit models
 so = sleuth_lrt(so, 'reduced', 'full')
 
 #extract the test results from the sleuth object 
